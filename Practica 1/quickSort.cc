7fa32e4d908cb6fd1230af688e73aac48132bd9c
obr6-ua <78750367+obr6-ua@users.noreply.github.com>
2024-02-15 17:18:31 +0000

    Nada

--- Practica 1/quickSort.cc
+++ Practica 1/quickSort.cc
@@ -29,3 +29,44 @@ middle_QuickSort(int * v, long left, long right){
     }
 }
 
+int main(void){
+
+    srand(0);
+
+    cout << "# QuickSort CPU times in milliseconds:"
+        << endl
+        << "# Size \t CPU time (ms.)"
+        << endl
+        << "# ----------------------------"
+        << endl;
+
+    for (int exp = 10; exp <= 16; exp++){
+        size_t size = size_t( pow(2,exp) );
+        int* v = new int [size];
+        if (!v){
+            cerr << "Error, not enough memory!" << endl;
+            exit (EXIT_FAILURE);  
+        }
+
+        cout << size << "\t\t" << std::flush;
+
+        for (size_t j = 0; j < size; j++) 
+            v[j] = rand(); 
+
+        auto start = clock();
+        middle_QuickSort(v,size);
+        auto end = clock();
+
+
+        cout << 1000.0 * (end-start) / CLOCKS_PER_SEC  << endl;
+
+        for (size_t i = 1; i < size; i++)
+            if (v[i] < v[i-1]){ 
+                cerr << "Panic, array not sorted! " << i << endl; 
+                exit(EXIT_FAILURE);            
+            }
+
+        delete[] v; 
+    }
+}
+
